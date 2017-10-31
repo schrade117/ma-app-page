1e04bc78d00b18d0481d5280aa68ee617fa55046
Asher Schraeter <asher.schraeter@gmail.com>
2017-10-31 14:19:43 -0400

    scripts/main.js

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    
+});
+
+
+$(document).ready(function(){
+
+   $('button').mouseover(function(){
+      $(this).addClass('hover');
+   });        
+   $('button').mouseout(function(){
+      $(this).removeClass('hover');
+   });
+});
+
+
